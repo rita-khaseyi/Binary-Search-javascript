3f520242013d425a6f08afa72dcf8ae0ed2f4b7d
rita khaseyi <ritakhaseyi@gmail.com>
2023-03-21 16:21:42 +0300

    first commit

--- binarySEarch.js
+++ binarySEarch.js
@@ -1,8 +1,7 @@
 function merge(left, right) {
     let arr = []
-    // Break out of loop if any one of the array gets empty
     while (left.length && right.length) {
-        // Pick the smaller among the smallest element of left and right sub arrays 
+    
         if (left[0] < right[0]) {
             arr.push(left.shift())  
         } else {
@@ -10,14 +9,13 @@ function merge(left, right) {
         }
     }
     
-    // Concatenating the leftover elements
-    // (in case we didn't go through the entire left or right array)
+    
     return [ ...arr, ...left, ...right ]
 }
 function mergeSort(array) {
     const half = array.length / 2
     
-    // Base case or terminating case
+   
     if(array.length < 2){
       return array 
     }
